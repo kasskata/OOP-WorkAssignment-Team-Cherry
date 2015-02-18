abd8c9c9d17edfff6d5cb802da65e29e6803af47
reathh <Ivelin Kirilov>
2015-02-18 14:33:26 +0200

    Added CookBook and StoreHouse classes

---
+++ ImTheOneWhoCooks/CookBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImTheOneWhoCooks.Contracts;
+
+namespace ImTheOneWhoCooks
+{
+    public class CookBook
+    {
+        List<IRecipe> recipes = new List<IRecipe>(); 
+    }
+}
---
+++ ImTheOneWhoCooks/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImTheOneWhoCooks.Contracts;
+
+namespace ImTheOneWhoCooks
+{
+    public class Inventory
+    {
+        List<IRecipe> products = new List<IRecipe>(); 
+    }
+}
